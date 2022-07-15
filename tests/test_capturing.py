adc6eb5734727f29d2c508de5be1564a703d3fa4
Josef Friedrich <josef@friedrich.rocks>
2022-07-15 22:03:51 +0200

    Test capturer

--- tests/test_capturing.py
+++ tests/test_capturing.py
@@ -3,9 +3,29 @@ import unittest
 
 from stdout_stderr_capturing import Capturing
 from termcolor import cprint
+from capturer import CaptureOutput
 
 
 class TestCapturing(unittest.TestCase):
+
+    def test_capturer(self):
+        with CaptureOutput() as capturer:
+            print('line 1')
+            print('line 2')
+        self.assertEqual(capturer.output.get_lines(), ['line 1', 'line 2'])
+
+    def test_capturer_stderr(self):
+        with CaptureOutput(merged=False) as capturer:
+            print('stderr', file=sys.stderr)
+            print('stdout')
+
+        self.assertEqual(capturer.stdout.get_text(), 'stdout')
+        self.assertEqual(capturer.stderr.get_text(), 'stderr')
+
+    def test_capturer_ansi(self):
+        with CaptureOutput() as capturer:
+            cprint('line 1', color='red')
+        self.assertEqual(capturer.output.get_text(), '\x1b[31mline 1\x1b[0m')
 
     def test_stdout(self):
         with Capturing() as output:

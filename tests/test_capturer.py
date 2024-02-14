82ee30e1eb7dbb85c45b4ed7871cf4c38339e28a
Josef Friedrich <josef@friedrich.rocks>
2024-02-14 14:41:44 +0100

    Fix all tests

--- tests/test_capturer.py
+++ tests/test_capturer.py
@@ -7,13 +7,13 @@ from termcolor import cprint
 
 @pytest.mark.skip("Fails on Github actions.")
 class TestCapturer:
-    def test_capturer(self):
+    def test_capturer(self) -> None:
         with CaptureOutput() as capturer:
             print("line 1")
             print("line 2")
         assert capturer.output.get_lines() == ["line 1", "line 2"]
 
-    def test_capturer_stderr(self):
+    def test_capturer_stderr(self) -> None:
         with CaptureOutput(merged=False) as capturer:
             print("stderr", file=sys.stderr)
             print("stdout")
@@ -21,7 +21,7 @@ class TestCapturer:
         assert capturer.stdout.get_text() == "stdout"
         assert capturer.stderr.get_text() == "stderr"
 
-    def test_capturer_ansi(self):
+    def test_capturer_ansi(self) -> None:
         with CaptureOutput() as capturer:
             cprint("line 1", color="red")
         assert capturer.output.get_text() == "\x1b[31mline 1\x1b[0m"
